96d7ecc397587374909cdbb732252dd850283a8f
Miles Lederman <miles@Miless-MacBook-Air.local>
2019-06-29 21:50:18 -0700

    cleaned up the input and got button inside

--- src/TextInput.js
+++ src/TextInput.js
@@ -1,18 +1,13 @@
 import React from 'react'
+import send from './send.png'
 
 class TextInput extends React.Component {
 
     render() {
-        return(<div className="text-input">     
-            <input/>
-            <button onClick = 'onButtonClick()'> SEND </button>        
+        return(<div contentEditable="true" className="text-input">     
+            <img src={send} className="send" alt='send'/>  
         </div>)
     }
-    
-    onButtonClick(){
-        alert("boop");
-    }
-
 }
 
 export default TextInput
