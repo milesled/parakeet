a4e8dc279a2b61d78aa8606d0fbf110ba81027eb
Miles Lederman <miles@Miless-MacBook-Air.local>
2019-06-29 21:16:23 -0700

    finished minimum requirements

--- src/TextInput.js
+++ src/TextInput.js
@@ -3,10 +3,14 @@ import React from 'react'
 class TextInput extends React.Component {
 
     render() {
-        return(<div className="text-input">
-            <input />
-            <button> SEND </button>
+        return(<div className="text-input">     
+            <input/>
+            <button onClick = 'onButtonClick()'> SEND </button>        
         </div>)
+    }
+    
+    onButtonClick(){
+        alert("boop");
     }
 
 }
