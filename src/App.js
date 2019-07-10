bc60b5b2213b73c5d8cf288e3b5a4bc7fbe20638
Miles Lederman <miles@D-10-19-238-217.dhcp4.washington.edu>
2019-07-10 16:36:00 -0700

    firebase troubleshooting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import tweet from './parakeet-alt.png'
 import TextInput from './TextInput'
 import NamePicker from './NamePicker'
+import * as firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/storage"
 
 class App extends React.Component {
 
@@ -13,20 +16,73 @@ class App extends React.Component {
     editName: false,
   }
 
-  gotMessage = (l) => {
+  /* <===========================> */
+
+  componentWillMount() {
+    var name = localStorage.getItem('name')
+    if (name)
+      this.setState({ name })
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyDtusB7wM5VMEnmSHBDzlBPA3kJXMR9X3Y",
+      authDomain: "parakeet-uw.firebaseapp.com",
+      projectId: "parakeet-uw",
+      storageBucket: "parakeet-uw.appspot.com"
+    });
+
+    this.db = firebase.firestore();
+
+    this.db.collection("messages").onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          //console.log(change.doc.data())
+          this.receive(change.doc.data())
+        }
+      })
+    })
+  }
+
+  /* <===========================> */
+
+  gotMessage = (m) => {
     const message = {
-      text: l,
+      text: m,
       from: this.state.name
     }
     var newMessagesArray = [message, ...this.state.messages]
     this.setState({ messages: newMessagesArray })
   }
 
+  setEditName = (editName) => {
+    if (!editName) {
+      localStorage.setItem('name', this.state.name)
+    }
+    this.setState({ editName })
+  }
+
+  /* <=========================> */
+
+  receive = (m) => {
+    const messages = [m, ...this.state.messages]
+    messages.sort((a, b) => b.ts - a.ts)
+    this.setState({ messages })
+  }
+  send = (m) => {
+    this.db.collection("messages").add({
+      ...m,
+      from: this.state.name || 'No name',
+      ts: Date.now()
+    })
+  }
+
+  /* <===========================> */
+
   render() {
-    var { messages } = this.state
+    var { name, messages } = this.state
     return (
       <div className="App">
         <header className="header">
+          <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
           <img src={tweet} className="logo" alt="" />
           Parakeet
       <div className="namePicker">
@@ -34,13 +90,14 @@ class App extends React.Component {
               name={this.state.name}
               editName={this.state.editName}
               changeName={name => this.setState({ name })}
-              setEditName={editName => this.setState({ editName })} />
+              setEditName={this.setEditName} />
           </div>
         </header>
         <main className="messages">
           {messages.map((m, i) => {
-            return <div key={i} className="bubble-wrap">
-                {m.from}
+            return <div key={i} className="bubble-wrap"
+              from={m.from === name ? "me" : "you"} >
+              {m.from !== name && <div className="bubble-name">{m.from}</div>}
               <div className="bubble">
                 <span>{m.text}</span>
               </div>
